46dfba2352a759c5f24a9add55b1d5df747781e6
student <student@ctt2033>
2018-02-12 14:43:53 +0200

    Hris commit.

--- onsitetho/src/onsitetho/st.java
+++ onsitetho/src/onsitetho/st.java
@@ -1,8 +1,12 @@
 package onsitetho;
 
 public class st {
-	public static void main(String[] args) {
-	}
 	
+	public static void main(String[] args) {
+		
+		System.out.println("Zdr bebce. Ko pr?");
+		
+		System.out.println("Rado e car! (master)");
+	}
 
 }
